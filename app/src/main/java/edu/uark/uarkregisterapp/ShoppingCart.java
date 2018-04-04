d3160e7f1785f5565c63e69b29e572943d3bf55c
jcconnol <jcconnol4@gmail.com>
2018-04-03 20:39:18 -0500

    added ShoppingCart activity with functionality and layout but no lists for products

---
+++ app/src/main/java/edu/uark/uarkregisterapp/ShoppingCart.java
@@ -0,0 +1,45 @@
+package edu.uark.uarkregisterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ShoppingCart extends AppCompatActivity {
+
+    Context fileContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+
+
+        try{
+            File file = new File(getString(R.string.internal_file_name));
+            FileInputStream fis = new FileInputStream(file);
+
+            //Construct BufferedReader from InputStreamReader
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                //things read from file go into list on screen
+
+
+            }
+
+            br.close();
+            //
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
